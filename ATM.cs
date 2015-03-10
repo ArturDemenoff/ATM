a59448fdd8a7fe04eb6a1d82ee7f53587c614397
ArturDemenoff <demen_off@mail.ru>
2015-03-10 23:27:44 +0300

    1

---
+++ ATM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    class ATM
+    {
+        public List<Bills> cassets = new List<Bills>();
+
+        public void loadCassets(string path, List<Bills> cassets)
+        {
+            using (var reader = new System.IO.StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    cassets.Add(new Bills() { Value = int.Parse(line.Split(' ')[0]), Count = int.Parse(line.Split(' ')[1]) });
+                }
+            }
+        }
+
+        public string getSumm(int cash)
+        {
+           
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            const int maxAmount = 5000000;
+
+            ATM atm = new ATM();
+
+            atm.loadCassets(@"C:\Users\Artur\Documents\Visual Studio 2013\Projects\OOP_lab1\OOP_lab1\money.txt", atm.cassets);
+            int summ;
+
+            do
+            {
+                Console.Write("Input your summ : ");
+                summ = int.Parse(Console.ReadLine());
+
+                if (summ != maxAmount)
+                {
+                    Console.Write(atm.getSumm(summ));
+                }
+                else
+                {
+                    break;
+                }
+
+            }while(summ != 0);           
+        }
+    }
+
+}
