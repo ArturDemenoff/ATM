500e4b282d5657cb36ffdca8be680d609b87c432
ArturDemenoff <demen_off@mail.ru>
2015-03-24 23:59:57 +0300

    up
    withDrawMoney

--- ATM.cs
+++ ATM.cs
@@ -3,30 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CashMachine
 {
     class ATM
     {
-        public List<Cassets> cassets = new List<Cassets>();
+        public Cassets[] cassets;
+        bool RightCombination = false;
 
         public void loadCassets(string path)
         {
-            using (var reader = new System.IO.StreamReader(path))
+            string[] stringCassets = File.ReadAllLines(path);
+
+            cassets = new Cassets[stringCassets.Length];
+
+            for(int i = 0; i < stringCassets.Length; i++)
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    cassets.Add(new Cassets() {Bill = new Bills() { Nominal = int.Parse(line.Split(' ')[0])}, Count = int.Parse(line.Split(' ')[1])});
-                }
+                cassets[i] = new Cassets() { Bill = new Bills() { Nominal = int.Parse(stringCassets[i].Split(' ')[0]) }, Count = int.Parse(stringCassets[i].Split(' ')[1]) };
             }
         }
-
-        public Money getSumm(int cash)
+        public void withdrawMoney()
         {
-            Money money = new Money();
+            int[] combination;
+            int requestedSum = 160;
 
-            return money;
+            for (int i = 1; i <= cassets.Sum(x => x.Count); i++)
+            {
+                if (RightCombination) break;
+                combination = new int[i];
+                FindCombination(cassets.Length, i, cassets, combination, requestedSum);
+            }
+        }
+        
+        public void FindCombination(int n,int k,Cassets[] cassets, int[] combination,int requestedSum, int combinationIndex = 0, int cassetsIndex = 0)
+        {
+            
+            if(combinationIndex == k)
+            {
+                if(combination.Sum() == requestedSum)
+                {
+                    RightCombination = true;
+                }
+            }
+            else 
+            {
+                for(int i = cassetsIndex; i < n; i++)
+                {
+                    if (RightCombination) break;
+                    combination[combinationIndex] = cassets[i].Bill.Nominal;
+                    FindCombination(n, k, cassets, combination, requestedSum, combinationIndex + 1, i);
+                }
+            }
         }
     }
 }
